fe16d2c146908619fff5ac8ed8d8d1f6220421e1
JD <imbajhey@gmail.com>
2022-04-18 11:19:36 +0800

    Add files via upload

---
+++ BalagwisJohnDenverFactorialRecursion/Program.cs
@@ -0,0 +1,42 @@
+﻿//JOhn Denver Balagwis
+//BSI/T- 2F
+//USing any programming Language create a program that print factorial number using recursion.
+
+using System;
+
+namespace BalagwisJohnDenverFactorialRecursion
+{
+    internal static class Program
+    {
+        //This part is the recursive  method
+        //This will find the factorial
+        //Here, we create a Factorial method with one parameter, which is the number for which we want to find the factorial.
+        //Inside the function,we will have a condition that will keep track of when to end the recursion.
+        private static int Factorial(int n)
+        {
+            //Here if the number is less than zero it will return -1 because n must be greater than or equal to 0.
+            if (n < 0)
+            {
+                return -1;
+            }
+            // Here is the condition that if number is 0 it will return  and simply terminate the condition
+            if (n == 0)
+            {
+                return 1;
+            }
+            //Here the recursive method call itself to calculate and return the factorial of the number.
+            return n * Factorial(n - 1);
+        }
+
+        private static void Main(string[] args)
+        {
+            //Here we use the Convert.ToInt32 function to convert the user input into an integer.
+            Console.Write("Enter any number: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            int fact = Factorial(value);
+            //Print the factorial of the number
+            Console.WriteLine($"\nFactorial of a number is: {fact}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BalagwisJohnDenverTowerofHanoi/Program.cs
@@ -0,0 +1,35 @@
+﻿//JOhn Denver Balagwis
+//BSI/T- 2F
+//Create a program for implementation in tower of Hanoi using any programming language.
+using System;
+
+namespace BalagwisJohnDenverTowerofHanoi
+{
+    internal static class Program
+    {
+        private static void TowerofHanoi(int disk, char source, char destination, char auxillary)
+        {
+            if (disk == 1)
+            {
+                Console.WriteLine($"\nMove Disc 1 from pole {source} to pole {destination}");
+            }
+            else
+            {
+                TowerofHanoi(disk - 1, source, auxillary, destination);
+                Console.WriteLine($"\nMove disk {disk} from pole {source} to pole {destination}");
+                TowerofHanoi(disk - 1, auxillary, destination, source);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            int numOfDisks;
+            Console.Write("Enter no. of Disks: ");
+            numOfDisks = Convert.ToInt32(Console.ReadLine());
+
+            //A is Source, B is Auxiliary  and C is for Destination
+            TowerofHanoi(numOfDisks, 'A', 'C', 'B');
+            Console.ReadLine();
+        }
+    }
+}
